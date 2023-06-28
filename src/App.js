5e022985db2c789dc883615f9cfda22529766fe5
howardjhkim <127804091+howardjhkim@users.noreply.github.com>
2023-06-27 22:01:09 -0700

    Implemented short break functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
 import Settings from "./Components/settings";
 import Timer from "./Components/timer";
+
+
 import {Context } from "./Context/DataContext";
 
 function App() {
   const {toggled} = useContext(Context);
-  console.log("toggled is: " + toggled);
+  // console.log("toggled is: " + toggled);
 
   return (
     <div className="App">
-      
-      <Timer />
+      <Timer/>
       <Settings />
     </div>
   );
--- src/Components/timer.js
+++ src/Components/timer.js
@@ -5,83 +5,158 @@ import timer from "../CSS/timer.css"
 function Timer() {
     //Context Provider
     const {timer, rounds, shortBreak, longBreak, toggled} = useContext(Context)
-    console.log(toggled)
 
-    const [endSign, setEndSign] = useState(false)
     const [isCounting, setIsCounting] = useState(false)
     const [intervalId, setIntervalId] = useState(null)
 
-    const [starting, setStarting] = useState(false)
 
     const [roundCounter, setRoundCounter] = useState(0)
 
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
 
-    const handleButtonClick = () => {
-        if(roundCounter > 0 && Number(roundCounter) === Number(rounds)) {
-            return
-        }
-        
-        if (isCounting) {
-            stopCounting();
-        } else {
-            startCounting();
-        }
-    }
+    //Break timer useStates
+
+    
+
+    
+    
+    const [counter, setCounter] = useState(0)
+    const [roundComplete, setRoundComplete] = useState(false)
+    const [breakTime, setBreakTime] = useState(false)
+    
+
+
+
+
+
+    console.log('breakTime: ' + breakTime)
+    console.log('isCounting: ' + isCounting)
+
+
+
 
     useEffect(() => {
+
+
         if (isCounting) {
-            if (minutes > 0 && seconds === 0) {
-                setMinutes((prevMins) => prevMins - 1)
-                setSeconds(59)
+            if (!breakTime) {
+                if (minutes > 0 && seconds === 0) {
+                    setMinutes((prevMins) => prevMins - 1)
+                    setSeconds(59)
+                }
+    
+                //when it reaches last second of the round | used so setMinutes does not rerender
+                if (counter > 0) {
+                    if (minutes === 0 && seconds === 0) {
+                        setRoundComplete(true)
+                        setBreakTime(true)
+                        setMinutes(shortBreak)
+                        setRoundCounter((prevRound) => prevRound + 1)
+                        stopFunc()            
+                    }
+                }
             }
 
-            if (minutes <= 0 && seconds === 0) {
-                setRoundCounter((prevRounds) => prevRounds + 1)
-                stopCounting()
-            }
 
-            if (minutes === 0 && seconds === 0) {
-                setMinutes(timer)
-                setStarting(false)
+            if (breakTime) {
+                if (minutes > 0 && seconds === 0) {
+                    setMinutes((prevMins) => prevMins - 1)
+                    setSeconds(59)
+                }
+    
+                //when it reaches last second of the round | used so setMinutes does not rerender
+                
+                if (minutes === 0 && seconds === 0) {
+                    setRoundComplete(false)
+                    setBreakTime(false)
+                    setMinutes(timer)
+                    stopFunc()            
+                }
             }
+        
+            ////////////////////////////////////////////////////
+
+
+        
+            
+
+
         }
     }, [seconds, timer, isCounting, setRoundCounter, minutes, rounds]);
 
-    const startCounting = () => {
-        if (!starting) {
-            setMinutes(timer)
+    const handleButtonClick = () => {
+        // if any of the settings are missing, return/do not run
+        if (!timer || !rounds || !shortBreak || !longBreak) {return} 
+
+        if(Number(roundCounter) === Number(rounds)) {return}
+
+   
+        if(!isCounting) {
+            startFunc()
+        } else {
+            stopFunc()
         }
         
-        setStarting(true)
+       
 
-        if(rounds.length === 0) {
-            return
+
+    }
+    // regular timer function
+    const regTimerFunc = () => {
+        if (counter === 0) {
+            setMinutes(timer)
         }
-        if (!isCounting) {
-            const id = setInterval(() => {
+        setCounter((prev) => prev + 1)
+        const id = setInterval(() => {
             setSeconds((prevSeconds) => prevSeconds - 1);
-            }, 1000);
-            setIntervalId(id);
-            setIsCounting(true);
-        }
-    };
+        }, 1000);
+        
+        setIntervalId(id);
+    }
 
-    const stopCounting = () => {
-        if (isCounting) {
-        clearInterval(intervalId);
-        setIntervalId(null);
-        setIsCounting(false)
+    // break timer function
+    const breakTimerFunc = () => {
+        
+        
+        setCounter((prev) => prev + 1)
+        const id = setInterval(() => {
+            setSeconds((prevSeconds) => prevSeconds - 1);
+        }, 1000);
+        
+        setIntervalId(id);
+
+
+       
+    }
+        
+    // general start counter function
+    const startFunc = () => {
+        setIsCounting(true)
+
+        // if timer is inputted and round is ready to go at round: 0/1, run the regular timer (regTimerFunc)
+        if (!breakTime) {
+            regTimerFunc();
+        }
+
+
+        if (breakTime) {
+            breakTimerFunc()
         }
     }
-    
+    // general stop counter function
+    const stopFunc = () => {
+        setIsCounting(false)
+        clearInterval(intervalId)
+    }
+
+
     return (
         <div className="timer-master" style={toggled ? { filter: 'blur(5px)' } : null}>
             <div className="title"><h1>domoporo</h1></div>
 
             <div className="focus-container">Round: {roundCounter} / {rounds.length === 0 ? '0' : rounds}</div>
-            <button className="timer" onClick={handleButtonClick}>
+            <button className="timer" onClick={handleButtonClick} style={roundComplete ? {display:'none'} : {}}>
                 <div className="time-container">
                     <div className="time">
                         {!timer.length ? `0` : `${minutes}`}
@@ -91,10 +166,23 @@ function Timer() {
                     <div className="pause-start">{isCounting ? "PAUSE" : "START"}</div>
                 </div>
             </button>
+
+
+            <button className="break-timer" onClick={handleButtonClick} style={!roundComplete ? {display:'none'} : {}}>
+                <div className="time-container">
+                    <div className="time">
+                        {!shortBreak.length ? `0` : `${minutes}`}
+                        :
+                        {seconds < 10 ? `0${seconds}` : `${seconds}`}
+                    </div>
+                    <div className="pause-start">{isCounting ? "PAUSE" : "START"}</div>
+                </div>
+            </button>
+
+
             <div className="category">
-                <div>Timer</div>
-                <div>Short Break</div>
-                <div>Long Break</div>
+                <div style={{backgroundColor: !breakTime ? `#E3686E` : ""}}>Timer</div>
+                <div style={{backgroundColor: breakTime ? `#E3686E` : ""}}>Short Break</div>
             </div>
         </div>
     )
